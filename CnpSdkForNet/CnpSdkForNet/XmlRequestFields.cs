a7e606ded4d16adb06e6fff6ca656021e354aa0f
e5651806 <sushamaghadage38@gmail.com>
2026-09-22 11:51:53 +0530

    commiting changes v12.50.1

--- CnpSdkForNet/CnpSdkForNet/XmlRequestFields.cs
+++ CnpSdkForNet/CnpSdkForNet/XmlRequestFields.cs
@@ -80,6 +80,7 @@ namespace Cnp.Sdk
         public translateToLowValueTokenRequest translateToLowValueTokenRequest;
         public realtimeIncrementalAuthorization realtimeIncrementalAuthorization;
         public queryDpoWalletBalance queryDpoWalletBalance;
+        public bool sameDayFunding;
 
         // Serialize the cnpOnlineRequest.
         // Convert the cnpOnlineRequest object to xml string.
@@ -87,7 +88,7 @@ namespace Cnp.Sdk
         {
             // Create header for the cnpOnlineRequest with user credential.
             var xml = "<?xml version='1.0' encoding='utf-8'?>\r\n<cnpOnlineRequest merchantId=\"" + merchantId
-                + "\" version=\"" + CnpVersion.CurrentCNPXMLVersion + "\" merchantSdk=\"" + merchantSdk + "\" xmlns=\"http://www.vantivcnp.com/schema\">"
+                + "\" version=\"" + CnpVersion.CurrentCNPXMLVersion + "\" merchantSdk=\"" + merchantSdk + "\" sameDayFunding=\"" + sameDayFunding.ToString().ToLower() + "\" xmlns=\"http://www.vantivcnp.com/schema\">"
                 + authentication.Serialize();
 
             // Because an online request can contain only one transaction, it assumes that only one instance variable of 
